1bf1793a2ded12306cb834c6f78235fab085e68f
Rakesh <Bisht8985@gmail.com>
2026-06-01 18:42:49 +0200

    Delete LOOPING & PATTERNS (ITERATION & FLOW)/Mathematical & Logical Patterns/check_arithematic_progression_print_first_n_term.py

--- LOOPING & PATTERNS (ITERATION & FLOW)/Mathematical & Logical Patterns/check_arithematic_progression_print_first_n_term.py
+++
@@ -1,8 +0,0 @@
-# Print first n terms of an arithmetic progression (a, d)
-
-a = int(input("\nenter first term: "))
-d = int(input("enter common diffrence number: "))
-n = int(input("enter number terms: "))
-
-for i in range(n):
-    print(a + i * d)
